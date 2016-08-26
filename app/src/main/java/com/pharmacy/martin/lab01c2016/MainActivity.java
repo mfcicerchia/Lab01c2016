78cc7b103a0b5496aaa6eac444e8c87e17776333
mfcicerchia <m.f.cicerchia@gmail.com>
2016-08-26 11:06:29 -0300

    agrego manejadores de eventos del boton confirmar

--- app/src/main/java/com/pharmacy/martin/lab01c2016/MainActivity.java
+++ app/src/main/java/com/pharmacy/martin/lab01c2016/MainActivity.java
@@ -1,18 +1,33 @@
 package com.pharmacy.martin.lab01c2016;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
 
 
-    TextView tvDias;
-    SeekBar sbCantDias;
 
+
+    protected TextView tvDias;
+    protected SeekBar sbCantDias;
+    protected Button btnConfirmar;
+    protected CheckBox chBoxRenovar;
+    protected EditText tfImporte;
+    protected EditText tfRendiminto;
+    protected TextView tvInfoConfirmacion;
+
+
+    protected double resultado;
+    protected double importe;
 
 
     @Override
@@ -20,50 +35,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvDias=(TextView) findViewById(R.id.tvDias);
-        sbCantDias=(SeekBar) findViewById(R.id.sbCantDias);
-        tvDias.setText("Dias: "+ sbCantDias.getProgress());
-
-        sbCantDias.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                tvDias.setText("Dias: "+ sbCantDias.getProgress());
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                tvDias.setText("Dias: "+ sbCantDias.getProgress());
-            }
-        });
+        /**Obtencion de los id's de los elementos de la Activity*/
+        tvDias = (TextView) findViewById(R.id.tvDias);
+        sbCantDias = (SeekBar) findViewById(R.id.sbCantDias);
+        btnConfirmar = (Button) findViewById(R.id.btnConfirmar);
+        chBoxRenovar = (CheckBox) findViewById(R.id.chBoxRenovar);
+        tfImporte = (EditText) findViewById(R.id.tfImporte);
+        tfRendiminto = (EditText) findViewById(R.id.tfRendimiento);
+        tvInfoConfirmacion = (TextView) findViewById(R.id.tvInfoConfirmacion);
 
 
+        /**inicializacion de los escuchadores de eventos*/
+        sbCantDias.setOnSeekBarChangeListener(this);
+        btnConfirmar.setOnClickListener((View.OnClickListener) this);
+
+
+        tvDias.setText("Dias: " + sbCantDias.getProgress());
     }
 
 
-
-/*    @Override
+    /**Manejadores de evento de la SeekBar*/
+    @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        tvDias.setText("Dias: "+ sbCantDias.getProgress());
-    }
+        tvDias.setText("Dias: " + sbCantDias.getProgress());
 
+
+        /**cambio el rendimiento mientras cambio la cantidad de dias*/
+        if (importe > 0) {
+            tfRendiminto.setText(String.valueOf(resultado));
+        } else {
+            tfRendiminto.setText(String.valueOf(0));
+        }
+
+    }
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
 
     }
 
+
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
-
-       *//* textView.setText("Covered: " + progress + "/" + seekBar.getMax());*//*
-        tvDias.setText("Dias: "+ sbCantDias.getProgress());
+        tvDias.setText("Dias: " + sbCantDias.getProgress());
     }
 
-    public void OnClickListener(View view){
 
-    }*/
+    /**Manejador de evento del Boton Confirmar*/
+    @Override
+    public void onClick(View v) {
+        tvInfoConfirmacion.setText(String.valueOf("El plazo fijo ha sido confirmadio, recibira $$$$ al finalizar el plazo"));
+    }
+
+
 }
