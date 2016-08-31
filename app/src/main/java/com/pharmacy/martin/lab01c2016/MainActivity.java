83b95767f6c4d6cb3f3c6f96ae7f98fff500480b
Pablo Paletto <pablo.paletto@gmail.com>
2016-08-31 17:05:48 -0300

    Implementado el calculo de interes y la comprobación de datos ingresados.

--- app/src/main/java/com/pharmacy/martin/lab01c2016/MainActivity.java
+++ app/src/main/java/com/pharmacy/martin/lab01c2016/MainActivity.java
@@ -4,6 +4,7 @@ import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -14,13 +15,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
-
-
-
 
     protected TextView tvDias;
     protected SeekBar sbCantDias;
@@ -30,10 +29,8 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     protected TextView tfRendiminto;
     protected TextView tvInfoConfirmacion;
 
-
     protected double resultado;
     protected double importe;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +43,6 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         btnConfirmar = (Button) findViewById(R.id.btnConfirmar);
         chBoxRenovar = (CheckBox) findViewById(R.id.chBoxRenovar);
         tfImporte = (EditText) findViewById(R.id.tfImporte);
-        tfRendiminto = (TextView) findViewById(R.id.tfRendimiento);
         tvInfoConfirmacion = (TextView) findViewById(R.id.tvInfoConfirmacion);
 
 
@@ -58,42 +54,40 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         tvDias.setText("Dias: " + sbCantDias.getProgress());
     }
 
-
     /**Manejadores de evento de la SeekBar*/
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         tvDias.setText("Dias: " + sbCantDias.getProgress());
-
-
-        /**cambio el rendimiento mientras cambio la cantidad de dias*/
-        if (importe > 0) {
-            tfRendiminto.setText(String.valueOf(resultado));
-        } else {
-            tfRendiminto.setText(String.valueOf(0));
-        }
-
     }
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
 
     }
 
-
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
         tvDias.setText("Dias: " + sbCantDias.getProgress());
     }
 
-
     /**Manejador de evento del Boton Confirmar*/
     @Override
     public void onClick(View v) {
-        /**Aca jugar con los colores verde y rojo*/
-        tvInfoConfirmacion.setTextColor(ContextCompat.getColor(this, R.color.correcto));
-        tvInfoConfirmacion.setText(String.valueOf("El plazo fijo ha sido confirmadio, recibira " + calcularGanancias() + " al finalizar el plazo"));
+        ArrayList<String> msgList = new ArrayList<>();
+        if(controlarDatos(msgList)){
+            tvInfoConfirmacion.setTextColor(ContextCompat.getColor(this, R.color.correcto));
+            tvInfoConfirmacion.setTextSize(28);
+            tvInfoConfirmacion.setText(String.valueOf("El plazo fijo ha sido confirmadio, recibira " + calcularGanancias() + " al finalizar el plazo"));
+        }
+        else{
+            tvInfoConfirmacion.setTextColor(ContextCompat.getColor(this, R.color.error));
+            tvInfoConfirmacion.setTextSize(28);
+            tvInfoConfirmacion.setText(String.valueOf(TextUtils.join("\n",msgList)));
+
+        }
     }
 
     private double calcularGanancias(){
+
         DecimalFormat resultado = new DecimalFormat("###.##");
         double interes, monto;
         float tasa,dias;
@@ -130,25 +124,42 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         interes = monto * ((Math.pow((1+tasa),(dias/360)))-1);
 
         return Double.parseDouble(resultado.format(interes + monto));
-//        this.importe = interes;
 
     }
 
     /*Metodo para comprobar que los datos ingresados sean correctos*/
-    private void controlarDatos(){
+    private boolean controlarDatos(ArrayList<String> msgList){
+        boolean state = true;
         EditText mail = (EditText)findViewById(R.id.editTextEmail);
         Pattern patMail = Pattern.compile("^[\\w-]+(\\.[\\w-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
         Matcher matEmail = patMail.matcher(mail.getText().toString());
         if(!matEmail.find()){
             //Si no es un mail valido
+            msgList.add(getResources().getText(R.string.errEmail).toString());
+            state = false;
         }
 
         EditText cuit = (EditText)findViewById(R.id.tfCuit);
-        Pattern patCuit = Pattern.compile("[20|23|27][0-9]{7,8}[0-9]");
-        Matcher matCuit = patMail.matcher(cuit.getText().toString());
+        Pattern patCuit = Pattern.compile("^(20|23|27)\\d{7,8}\\d$");
+        Matcher matCuit = patCuit.matcher(cuit.getText().toString());
         if(!matCuit.find()){
-        //Si no es un cuit valido
+            //Si no es un cuit valido
+            msgList.add(getResources().getText(R.string.errCuit).toString());
+            state = false;
         }
+        return state;
+    }
+
+    private String arrayToString(ArrayList<String> lista){
+        StringBuilder sbResultado = new StringBuilder();
+        String resutlado=null;
+
+        for (String s : lista){
+            sbResultado.append(s);
+            sbResultado.append("\t");
+        }
+
+        return sbResultado.toString();
     }
 
 }
