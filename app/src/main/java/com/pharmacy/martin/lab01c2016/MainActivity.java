95c55e1ef2dad0d4b52c8a72a2170616cd586a0f
Pablo Paletto <pablo.paletto@gmail.com>
2016-08-31 15:43:47 -0300

    Cambie el color secundario de un "lila bala" a un "azul clasico" y puse algunas mayusculas.

--- app/src/main/java/com/pharmacy/martin/lab01c2016/MainActivity.java
+++ app/src/main/java/com/pharmacy/martin/lab01c2016/MainActivity.java
@@ -1,6 +1,7 @@
 package com.pharmacy.martin.lab01c2016;
 
 import android.net.wifi.p2p.WifiP2pManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -12,6 +13,10 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
 
 
@@ -22,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     protected Button btnConfirmar;
     protected CheckBox chBoxRenovar;
     protected EditText tfImporte;
-    protected EditText tfRendiminto;
+    protected TextView tfRendiminto;
     protected TextView tvInfoConfirmacion;
 
 
@@ -41,7 +46,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         btnConfirmar = (Button) findViewById(R.id.btnConfirmar);
         chBoxRenovar = (CheckBox) findViewById(R.id.chBoxRenovar);
         tfImporte = (EditText) findViewById(R.id.tfImporte);
-        tfRendiminto = (EditText) findViewById(R.id.tfRendimiento);
+        tfRendiminto = (TextView) findViewById(R.id.tfRendimiento);
         tvInfoConfirmacion = (TextView) findViewById(R.id.tvInfoConfirmacion);
 
 
@@ -84,12 +89,66 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     @Override
     public void onClick(View v) {
         /**Aca jugar con los colores verde y rojo*/
-        tvInfoConfirmacion.setText(String.valueOf("El plazo fijo ha sido confirmadio, recibira $$$$ al finalizar el plazo"));
+        tvInfoConfirmacion.setTextColor(ContextCompat.getColor(this, R.color.correcto));
+        tvInfoConfirmacion.setText(String.valueOf("El plazo fijo ha sido confirmadio, recibira " + calcularGanancias() + " al finalizar el plazo"));
     }
 
-    private void calcularGanancias(){
-        this.importe =0;
+    private double calcularGanancias(){
+        DecimalFormat resultado = new DecimalFormat("###.##");
+        double interes, monto;
+        float tasa,dias;
+        EditText auxMonto = (EditText) findViewById(R.id.tfImporte);
+        SeekBar auxDias = (SeekBar) findViewById(R.id.sbCantDias);
+        monto = Integer.parseInt(auxMonto.getText().toString());
+        dias = auxDias.getProgress();
+        if(monto < 5000){
+            if(dias<30){
+                tasa=getResources().getFraction(R.fraction.interes5000_Menor30,1,1);
+            }
+            else{
+                tasa=getResources().getFraction(R.fraction.interes5000_Mayor30,1,1);
+            }
+        }else {
+            if(monto < 99999){
+                if(dias<30){
+                    tasa=getResources().getFraction(R.fraction.interes99999_Menor30,1,1);
+                }
+                else{
+                    tasa=getResources().getFraction(R.fraction.interes99999_Mayor30,1,1);
+                }
+            }
+            else{
+                if(dias<30){
+                    tasa=getResources().getFraction(R.fraction.interesMayor99999_Menor30,1,1);
+                }
+                else{
+                    tasa=getResources().getFraction(R.fraction.interesMayor99999_Mayor30,1,1);
+                }
+            }
+        }
 
+        interes = monto * ((Math.pow((1+tasa),(dias/360)))-1);
+
+        return Double.parseDouble(resultado.format(interes + monto));
+//        this.importe = interes;
+
+    }
+
+    /*Metodo para comprobar que los datos ingresados sean correctos*/
+    private void controlarDatos(){
+        EditText mail = (EditText)findViewById(R.id.editTextEmail);
+        Pattern patMail = Pattern.compile("^[\\w-]+(\\.[\\w-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Matcher matEmail = patMail.matcher(mail.getText().toString());
+        if(!matEmail.find()){
+            //Si no es un mail valido
+        }
+
+        EditText cuit = (EditText)findViewById(R.id.tfCuit);
+        Pattern patCuit = Pattern.compile("[20|23|27][0-9]{7,8}[0-9]");
+        Matcher matCuit = patMail.matcher(cuit.getText().toString());
+        if(!matCuit.find()){
+        //Si no es un cuit valido
+        }
     }
 
 }
